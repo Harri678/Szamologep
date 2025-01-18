2f39e839923339f4bca604599eb811ea2f88f7b4
Máté Harrer <harrer.mate@petrik.hu>
2025-01-18 10:21:56 +0100

    indit kesz

--- Szamologep/Program.cs
+++ Szamologep/Program.cs
@@ -16,20 +16,54 @@ namespace Szamologep
         {
             return a - b;
         }
-        public double osztas(double a, double b)
+        public double Osztas(double a, double b)
         {
             return a / b;
         }
-        public double szorzas(double a, double b)
+        public double Szorzas(double a, double b)
         {
             return a * b;
         }
-        public double szazalek(double a, double b)
+        public double Szazalek(double a, double b)
         {
             return a * b / 100;
         }
+
+        public void Indit()
+        {
+            Console.WriteLine("Kerem adja meg az elso szamot: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Kerem adja meg a masodik szamot: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Az elvegezendo muvelet jele: ");
+            string muvelet = Console.ReadLine();
+            switch (muvelet)
+            {
+                case "+":
+                    Console.WriteLine(Osszeadas(a, b));
+                    break;
+                case "-":
+                    Console.WriteLine(Kivonas(a, b));
+                    break;
+                case "*":
+                    Console.WriteLine(Szorzas(a, b));
+                    break;
+                case "/":
+                    Console.WriteLine(Osztas(a, b));
+                    break;
+                case "%":
+                    Console.WriteLine(Szazalek(a, b));
+                    break;
+                default:
+                    Console.WriteLine("Hibas muvelet");
+                    break;
+            }
+        }
+
         static void Main(string[] args)
         {
+            Program program = new Program();
+            program.Indit();
         }
     }
 }
